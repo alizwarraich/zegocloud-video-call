9b8f024c702d1ba9288dafade45153b375545f10
Ali Zulqarnain <warraichleopard@gmail.com>
2023-03-29 16:45:02 +0500

    Final

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 export default function Home() {
+    const [roomID, setRoomID] = React.useState("");
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>Welcome to the Video Chat App powered by Zego</h1>
+            <form
+                action={`/room/${roomID}`}
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: 10,
+                }}
+            >
+                <input
+                    type="text"
+                    placeholder="Enter room id"
+                    value={roomID}
+                    onChange={(e) => setRoomID(e.target.value)}
+                />
+                <button style={{ width: 100 }}>Join Room</button>
+            </form>
         </div>
     );
 }
--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -4,9 +4,35 @@ import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
 function Room() {
     const { roomID } = useParams();
+    const MyMeeting = async (element) => {
+        const appID = import.meta.env.VITE_ZEGOCLOUD_APP_ID;
+        const serverSecret = import.meta.env.VITE_ZEGOCLOUD_SERVER_SECRET;
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            Number(appID),
+            serverSecret,
+            roomID,
+            Date.now().toString(),
+            "Ali Warraich"
+        );
+        const zegoCloud = ZegoUIKitPrebuilt.create(kitToken);
+        zegoCloud.joinRoom({
+            container: element,
+            scenario: {
+                mode: ZegoUIKitPrebuilt.OneONoneCall,
+            },
+            sharedLinks: [
+                {
+                    title: "MedxCure Video Call",
+                    url: `http://localhost:5173/room/${roomID}`,
+                },
+            ],
+            showScreenSharingButton: false,
+        });
+    };
+
     return (
         <div>
-            <h1>Room: {roomID}</h1>
+            <div ref={MyMeeting} style={{ width: "98vw", height: "97vh" }} />
         </div>
     );
 }
